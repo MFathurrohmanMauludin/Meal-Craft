83d15d129a934fa47a8c0e6c3bf70ce90961dcba
M Fathurrohman Mauludin <54023601+MFathurrohmanMauludin@users.noreply.github.com>
2022-01-25 18:38:03 +0700

    Add files via upload
    update

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// import module main.js
+import "regenerator-runtime";
+import "./styles/style.css";
+import "./script/component/app-meal.js";
+import main from "./script/view/main.js";
+
+document.addEventListener("DOMContentLoaded", main);
---
+++ src/script/component/app-meal.js
@@ -0,0 +1,38 @@
+class AppMeal extends HTMLElement {
+
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({
+            mode: "open"
+        });
+    }
+
+    connectedCallback() {
+        this.render();
+    }
+
+    render() {
+        this.shadowDOM.innerHTML = `
+        <style>
+            * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+            }
+            :host {
+                display: block;
+                width: 100%;
+                background-image: linear-gradient(to right, #8360c3, #2ebf91);
+                color: white;
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            }
+            
+            h2 {
+                padding: 28px;
+            }
+        </style>
+        <h2>Mealcraft Apps</h2>`;
+    }
+}
+
+customElements.define("app-meal", AppMeal);
